16ea8cfdf6109b23b8cc93c284fe3ec177935f7a
ItsMateo20 <mateostar2009@gmail.com>
2023-12-06 16:44:08 +0100

    Admin account + code cleanup [UPDATE]

--- database.js
+++ database.js
@@ -4,27 +4,45 @@ const User = require('./src/models/User.js');
 const UserSettings = require('./src/models/UserSettings.js');
 const Whitelisted = require('./src/models/Whitelisted.js');
 
+async function connectDatabase() {
+    const sequelize = new Sequelize({
+        dialect: 'sqlite',
+        logging: false,
+        storage: 'database.sqlite',
+    });
+
+    try {
+        await sequelize.authenticate();
+        console.log(gray('[SITE]: ') + cyan('Database connected!'));
+        return sequelize;
+    } catch (error) {
+        console.error(gray('[SITE]: ') + red('Database connection error:'), error);
+        throw error;
+    }
+}
+
+async function synchronizeModels(sequelize) {
+    try {
+        await User.sync({ alter: true });
+        console.log(gray('[SITE]: ') + cyan('User model synchronized successfully!'));
+
+        await UserSettings.sync({ alter: true });
+        console.log(gray('[SITE]: ') + cyan('UserSettings model synchronized successfully!'));
+
+        await Whitelisted.sync({ alter: true });
+        console.log(gray('[SITE]: ') + cyan('Whitelisted model synchronized successfully!'));
+    } catch (error) {
+        console.error(gray('[SITE]: ') + red('Error synchronizing models:'), error);
+        throw error;
+    }
+}
+
 module.exports = {
     name: 'database',
     description: 'Database',
     async execute() {
-        const sequelize = new Sequelize({
-            dialect: 'sqlite',
-            logging: false,
-            storage: 'database.sqlite',
-        });
-
-        await User.sync({
-            alter: true
-        })
-
-        await UserSettings.sync({
-            alter: true
-        })
-
-        await Whitelisted.sync({
-            alter: true
-        })
+        const sequelize = await connectDatabase();
+        await synchronizeModels(sequelize);
 
         const adminFind = await User.findOne({ where: { email: 'admin@localhost' } })
         if (!adminFind) {
@@ -36,13 +54,5 @@ module.exports = {
             })
             await UserSettings.create({ email: 'admin@localhost' })
         }
-
-        await sequelize.authenticate()
-            .then(() => {
-                console.log(gray('[SITE]: ') + cyan('Database connected!'));
-            })
-            .catch((error) => {
-                console.error(gray('[SITE]: ') + red('Database was unable to connect doe to an error:'), error);
-            });
     }
 }
